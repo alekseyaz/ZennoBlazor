60ad3bd936e354eabbd92b599486296842585987
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2020-05-05 15:53:13 +0300

    ...

--- ZennoBlazor/Server/Startup.cs
+++ ZennoBlazor/Server/Startup.cs
@@ -65,7 +65,7 @@ namespace ZennoBlazor.Server
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                //app.UseHsts();
+                app.UseHsts();
             }
 
             logger.LogInformation($"env.IsDevelopment():- {env.IsDevelopment()}");
